561b55d80157f44bfed5087c776b838969114f0e
code735 <code735@gmail.com>
2012-05-04 12:25:20 +0100

    added more contacts and info for about page

--- youthcamp/application/views/pages/contact.php
+++ youthcamp/application/views/pages/contact.php
@@ -4,26 +4,36 @@
 			<table class='table table-bordered table-striped'>
 		    <thead>
 			    <tr>
+			    	<th>Role</th>
 				    <th>Contact Person </th>
 				    <th>Phone Number </th>
 			    </tr>
 		    </thead>
 		    <tbody>
 			   <tr>
+			   		<td>Registration</td>
 					<td>Andrew Amegatcher</td>
 					<td>0267666975</td>
 				</tr>
 				<tr>
+					<td>Registration</td>
 					<td>Anthony Dzixose-Davor</td>
 					<td>0244226692</td>
 				</tr>
 				<tr>
+					<td></td>
 					<td>Babatunde Ampah</td>
 					<td>0264055555</td>
 				</tr>
 				<tr>
+					<td>Transport</td>
 					<td>Sipho Twum</td>
 					<td>0244233994</td>
+				</tr>
+				<tr>
+					<td>Payments</td>
+					<td>Kwamena Odum </td>
+					<td>0244835721</td>
 				</tr>
 		    </tbody>
 	    </table>
