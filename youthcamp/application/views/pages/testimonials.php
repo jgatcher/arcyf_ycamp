970abecef7ea6e20e7c2b0987cc487820b597ce4
code735 <code735@gmail.com>
2012-05-18 00:37:30 +0000

    refactorings and new content

---
+++ youthcamp/application/views/pages/testimonials.php
@@ -0,0 +1,36 @@
+<div class='  testimonials'>
+			<div class="row">
+
+				<div class="test span3 polaroid pull-left">
+					
+					<div>
+						<img src="<?php echo base_url() ?>/assets/images/abokobi.jpg" width='270' height ='120'>
+					</div>
+					<h3>Experience 1</h3>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat.
+				</div>
+				<div class="test span3 polaroid ">
+					<div>
+						<img src="<?php echo base_url() ?>/assets/images/abokobi.jpg" width='270' height ='120'>
+					</div>
+					<h3>Experience 3</h3>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. 
+				</div>
+				<div class="test span3 polaroid pull-left">
+					<div>
+						<img src="<?php echo base_url() ?>/assets/images/hz.jpg" width='270' height ='120'>
+					</div>
+					<h3>Experience 3</h3>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. 
+				</div>
+			</div>
+</div>
