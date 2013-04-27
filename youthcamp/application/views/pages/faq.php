a23d8d8dff7a6a53b63acdacec3563f477e6c222
selasiehanson <soundfever18@gmail.com>
2013-04-27 17:53:18 +0000

    modified faqs

--- youthcamp/application/views/pages/faq.php
+++ youthcamp/application/views/pages/faq.php
@@ -6,11 +6,11 @@
 			<ol>
 				<li>
 					<div class='question'>How much will it cost to attend camp this year?</div>
-					<div class='answer'>This year’s camp will cost each camper a highly subsidised fee of GHC60.</div>
+					<div class='answer'>This year’s camp will cost each camper a highly subsidised fee of GHC70.</div>
 				</li>
 				<li>
 					<div class='question'>When is the deadline for registration?</div>
-					<div class='answer'>Deadline for registration is 10th June 2012. 
+					<div class='answer'>Deadline for registration is 8th June 2013. 
 						Registration is only complete upon payment and failure to make payment 
 						before the registration deadline will result in a forfeiture of registrant’s place at camp. 
 						To avoid this situation, campers are required to inform registration officers beforehand of 
@@ -23,19 +23,19 @@
 						to Friday and on Sundays at registration desk.
 					</div>
 				</li>
-				<li>
+				<!-- <li>
 					<div class='question'>Are there any discounts for families with multiple members attending camp?</div>
 					<div class='answer'>  
 						Yes, families with 3 or more members attending camp will receive a 15% discount 
 						on the total amount they would have had to pay.
 					</div>
-				</li>
+				</li> -->
 				<!-- <li>
 					<div class='question'>What do I do if I want to attend camp but can’t afford it?</div>
 					<div class='answer'>There is currently no provision for sponsorship. However campers who</div>
 				</li> -->
 				<li>
-					<div class='question'>Will I still have to pay GHC60 if I don’t make it on Wednesday?</div>
+					<div class='question'>Will I still have to pay GHC70 if I don’t make it on Wednesday?</div>
 					<div class='answer'> Campers who will be at camp anytime between Thursday (14th June ) and Saturday (16th June) 
 						will be required to pay a camp fee of GHC50.
 					</div>
