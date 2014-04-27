bf09514856e74402228960699c55cb662fd7af8a
jgatcher <jgatcher@gmail.com>
2014-04-27 12:54:49 +0000

    Update mongo_db.php

--- youthcamp/application/config/mongo_db.php
+++ youthcamp/application/config/mongo_db.php
@@ -9,18 +9,19 @@
 //$config['mongo_persist_key'] = 'ci_mongo_persist';
 
 
-//$config['mongo_host'] = 'ds031857.mongolab.com:31857/youthcamp_dev';
-//$config['mongo_port'] = '';
-//$config['mongo_db'] = 'youthcamp2_dev';
-//$config['mongo_user'] = 'soundfever';
-//$config['mongo_pass'] = 'soundfever18';
-//$config['mongo_persist'] = TRUE;
-//$config['mongo_persist_key'] = 'ci_mongo_persist';
-
+$config['mongo_host'] = 'ds033767.mongolab.com:33767/youthcamp14_dev ';
+$config['mongo_port'] = '';
+$config['mongo_db'] = 'youthcamp14_dev';
+$config['mongo_user'] = 'soundfever';
+$config['mongo_pass'] = 'soundfever18';
+$config['mongo_persist'] = TRUE;
+$config['mongo_persist_key'] = 'ci_mongo_persist';
+/*
  $config['mongo_host'] = 'ds041347.mongolab.com:41347/youthcamp13_live';
  $config['mongo_port'] = '';
- $config['mongo_db'] = 'youthcamp13_live';
+ $config['mongo_db'] = 'youthcamp14_live';
  $config['mongo_user'] = 'arcyf';
  $config['mongo_pass'] = 'connecting';
 $config['mongo_persist'] = TRUE;
  $config['mongo_persist_key'] = 'ci_mongo_persist';
+*/
